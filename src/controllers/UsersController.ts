d343671531a0ec743db3f4cdf802a7b262697149
djaldim <diego@jettax.com.br>
2022-10-06 11:21:28 -0300

    Create users controllers methods

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -5,9 +5,18 @@ class UsersController {
 
   public async list(req: Request, res: Response): Promise<Response> {
     const users = await Users.find();
-    console.log('users:', users);
+    return res.json(users);
+  }
 
-    return res.json({message: 'List users lkasjlajsdlkj'});
+  public async create(req: Request, res: Response): Promise<Response> {
+    const user = await Users.create(req.body);
+    return res.json(user);
+  }
+
+  public async getById(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const user = await Users.findById(id);
+    return res.json(user);
   }
 
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,5 +4,7 @@ import UsersController from '@controllers/UsersController';
 const routes = Router();
 
 routes.get('/', UsersController.list);
+routes.post('/', UsersController.create);
+routes.get('/:id', UsersController.getById);
 
 export default routes;
