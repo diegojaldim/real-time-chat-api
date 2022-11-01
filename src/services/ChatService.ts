c818ea14ab873b42950a1fa38e483fdfa4bbdad8
djaldim <diego@jettax.com.br>
2022-11-01 11:27:40 -0300

    Check if recipient and sender exists

--- src/services/ChatService.ts
+++ src/services/ChatService.ts
@@ -1,6 +1,8 @@
 import Chat from '@entities/Chat';
+import User from '@entities/User';
 import ChatRepository from '@repositories/ChatRepository';
 import { Types } from 'mongoose';
+import UserService from '@services/UserService';
 
 class ChatService {
 
@@ -10,9 +12,21 @@ class ChatService {
     this.chatRepository = new ChatRepository();
   }
 
-  public sendMessage(chat: Chat): Promise<Chat> {
+  public async sendMessage(chat: Chat): Promise<Chat> {
     if (this.isSenderAndRecipientEquals(chat.recipient._id.toString(), chat.sender._id.toString())) {
       throw new Error('Não pode enviar mensagem para você mesmo!');
+    }
+
+    const isRecipientExists = await this.isUserExists(chat.recipient.toString());
+
+    if (!isRecipientExists) {
+      throw new Error('Destinatário não existente');
+    }
+
+    const isSenderExists = await this.isUserExists(chat.recipient.toString());
+  
+    if (!isSenderExists) {
+      throw new Error('Remetente não existente');
     }
 
     return this.chatRepository.create(chat);
@@ -29,9 +43,15 @@ class ChatService {
     return await this.chatRepository.channel(recipient, sender);
   }
 
-  public isSenderAndRecipientEquals(recipient: string, sender: string): boolean {
+  private isSenderAndRecipientEquals(recipient: string, sender: string): boolean {
     return sender === recipient;
   }
+
+  private async isUserExists(userId: string): Promise<User> {
+    const userService: UserService = new UserService();
+    return await userService.getById(userId);
+  }
+
 }
 
 export default ChatService;
